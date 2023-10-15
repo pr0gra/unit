5959ba18225e9a807d8c0d069714d9d4f3f4f4e0
Vi <71643953+vi-mp3@users.noreply.github.com>
2023-10-15 19:11:00 +0000

    регистрация и вход

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -32,52 +32,48 @@ export default function Registration() {
     });
 
   return (
-    <div className="">
-      <form onSubmit={handleSubmit} className="">
-        <h2 className="">Регистрация</h2>
-        <div className="">
+    <div className="bg-gray-500 pb-20">
+      <div className="text-4xl text-white py-8 flex justify-center">юнит</div>
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-lg w-full bg-white mx-auto rounded-2xl px-12 pb-12"
+      >
+        <div className="flex justify-center py-8 text-3xl">Регистрация</div>
+        <div className="space-y-6">
           <input
-            className=""
+            className="border border-neutral-300 rounded-2xl bg-neutral-200 py-4 w-full pl-12"
             type="text"
             id="name"
             placeholder="Имя"
             onChange={handleChange}
             value={values.name}
           />
-        </div>
-        <div className="">
           <input
-            className=""
+            className="border border-neutral-300 rounded-2xl bg-neutral-200 py-4 w-full pl-12"
             type="text"
             id="surname"
             placeholder="Фамилия"
             onChange={handleChange}
             value={values.surname}
           />
-        </div>
-        <div className="">
           <input
-            className=""
+            className="border border-neutral-300 rounded-2xl bg-neutral-200 py-4 w-full pl-12"
             type="text"
             id="email"
             placeholder="Электронная почта"
             onChange={handleChange}
             value={values.email}
           />
-        </div>
-        <div className="">
           <input
-            className=""
+            className="border border-neutral-300 rounded-2xl bg-neutral-200 py-4 w-full pl-12"
             type="text"
             id="phoneNumber"
             placeholder="Номер телефона"
             onChange={handleChange}
             value={values.phoneNumber}
           />
-        </div>
-        <div className="">
           <input
-            className=""
+            className="border border-neutral-300 rounded-2xl bg-neutral-200 py-4 w-full pl-12"
             type="text"
             id="birthdayDate"
             placeholder="Дата рождения"
@@ -85,10 +81,13 @@ export default function Registration() {
             value={values.birthdayDate}
           />
         </div>
-        <p className="">
+        <div className="mt-6">
           <input type="checkbox" /> Ознакомлен с политикой конфиденциальности
-        </p>
-        <button typeof="submit" className="">
+        </div>
+        <button
+          typeof="submit"
+          className="bg-black rounded-full text-white w-full py-4 mt-8 mb-2"
+        >
           Зарегистрироваться
         </button>
       </form>
