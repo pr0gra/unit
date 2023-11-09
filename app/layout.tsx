799b086a7b466f87f73a97b2b51db7d98c0fe967
Vi <71643953+vi-mp3@users.noreply.github.com>
2023-11-09 19:20:21 +0000

    обновил футер

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 const inter = Inter({
   subsets: ["latin", "cyrillic"],
@@ -89,112 +90,35 @@ export default function RootLayout({
           </>
         )}
         {children}
-        <div className="bg-black flex justify-center py-8">
-          <svg
-            width="86"
-            height="98"
-            viewBox="0 0 86 98"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g clip-path="url(#clip0_9_52)">
-              <path
-                d="M1.61328 24.5848L43.0513 1.89844"
-                stroke="white"
-                stroke-width="3"
-                stroke-linecap="round"
-              />
-              <path
-                d="M84.3872 73.7539L42.9492 96.4404"
-                stroke="white"
-                stroke-width="3"
-                stroke-linecap="round"
-              />
-              <path
-                d="M1.61328 25.9209H84.3883"
-                stroke="white"
-                stroke-width="3"
-                stroke-linecap="square"
-              />
-              <path
-                d="M1.61328 72.418H84.3883"
-                stroke="white"
-                stroke-width="3"
-                stroke-linecap="square"
-              />
-              <path
-                d="M84.3867 25.9209V72.4178"
-                stroke="white"
-                stroke-width="3"
-                stroke-linecap="square"
-              />
-              <path
-                d="M43 72.4177V95.1317"
-                stroke="white"
-                stroke-width="3"
-                stroke-linecap="square"
-              />
-              <path
-                d="M1.61329 25.9209L1.61328 72.4178"
-                stroke="white"
-                stroke-width="3"
-                stroke-linecap="square"
-              />
-              <path
-                d="M43.2148 1.88593L84.4029 24.6383"
-                stroke="white"
-                stroke-width="3"
-                stroke-linecap="round"
-              />
-              <path
-                d="M42.7859 96.4527L1.59766 73.7002"
-                stroke="white"
-                stroke-width="3"
-                stroke-linecap="round"
-              />
-              <path
-                d="M84.1036 71.2451L63.8242 26.3506"
-                stroke="white"
-                stroke-width="3"
-                stroke-linecap="round"
-              />
-              <path
-                d="M42.4474 71.2451L22.168 26.3506"
-                stroke="white"
-                stroke-width="3"
-                stroke-linecap="round"
-              />
-              <path
-                d="M43.6797 70.6473L63.7945 26.8483"
-                stroke="white"
-                stroke-width="3"
-                stroke-linecap="round"
-              />
-              <path
-                d="M2.29297 70.6472L22.4079 26.8481"
-                stroke="white"
-                stroke-width="3"
-                stroke-linecap="round"
-              />
-              <path
-                d="M44.0879 2.56641L63.8569 25.9087"
-                stroke="white"
-                stroke-width="3"
-                stroke-linecap="round"
-              />
-              <path
-                d="M43.1493 1.88379L22.8574 25.5054"
-                stroke="white"
-                stroke-width="3"
-                stroke-linecap="round"
-              />
-            </g>
-            <defs>
-              <clipPath id="clip0_9_52">
-                <rect width="86" height="97.8039" fill="white" />
-              </clipPath>
-            </defs>
-          </svg>
+        <div className="bg-black text-white">
+          <div className="max-w-7xl mx-auto pt-10 flex justify-between text-sm px-8">
+            <div className="w-[500px]">
+              <div className="text-3xl font-bold mb-4">
+                юнит
+              </div>
+              <div>
+                ЮНИТ - первое студенческое IT объединение в Екатеринбурге,
+                которое базируется в Уральском Экономическом университете
+                (УРГЭУ).
+              </div>
+              <div>
+                Мы верим, что будущее можно создать вместе и реальные проекты
+                разрабатывать уже будучи студентами
+              </div>
+            </div>
+            <div className="w-[356px]">
+              <div className="font-bold mb-2 mt-4">Документы</div>
+              <div className="mb-12">
+                Политика обработки персональных данных
+              </div>
+              <div className="font-bold mb-4">Мы в социальных сетях</div>
+              <div className="flex gap-3 mb-20">
+                <Image alt="" src="/vk.png" height={40} width={40} />
+                <Image alt="" src="/tg.png" height={40} width={40} />
+                <Image alt="" src="/yt.png" height={40} width={40} />
+              </div>
+            </div>
+          </div>
         </div>
       </body>
     </html>
